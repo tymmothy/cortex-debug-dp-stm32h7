a64fb91dca9688f685f4b56e7c41887713fc64ae
Tymm Zerr <345123+tymmothy@users.noreply.github.com>
2023-12-02 15:31:54 -0800

    Updated package with current set of SVD files in STM32CubeIDE

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# Cortex-Debug: Device Support Pack - STM32H7
+# Cortex-Debug: Device Support Pack - STM32H7 (forked)
 
 This is an extension compatible with Cortex-Debug > v0.3.0, adds the necessary
 SVD files for the STMicroelectronics STM32H7 MCU family.
+
+This is a fork of https://github.com/jeandudey/cortex-debug-dp-stm32h7 that has
+been updated with current SVD files for H7 processors from the STM32CubeIDE.
+
+Note: The silicon revision "V" files have been removed
--- src/extension.ts
+++ src/extension.ts
@@ -6,22 +6,34 @@ interface CortexDebug {
 }
 
 export function activate(context: ExtensionContext) {
-        console.log('Congratulations, your extension "helloworld-sample" is now active!');
-
     const cortexDebug: Extension<CortexDebug> = <Extension<CortexDebug>>extensions.getExtension('marus25.cortex-debug');
     if (!cortexDebug) {
         window.showErrorMessage('Cortex-Debug Extension is not available for device support packages');
         return;
     }
+        window.showErrorMessage('Cortex-Debug Extension is not available for device support packages');
 
     cortexDebug.activate().then((cdbg) => {
-        cdbg.registerSVDFile(/^(STM|stm)32(h|H)743.*/i, path.join(context.extensionPath, 'data', 'stm32h743.svd'));
-        cdbg.registerSVDFile(/^(STM|stm)32(h|H)743v.*/i, path.join(context.extensionPath, 'data', 'stm32h743v.svd'));
-        cdbg.registerSVDFile(/^(STM|stm)32(h|H)747(CM|cm)4.*/i, path.join(context.extensionPath, 'data', 'stm32h747cm4.svd'));
-        cdbg.registerSVDFile(/^(STM|stm)32(h|H)747(CM|cm).*/i, path.join(context.extensionPath, 'data', 'stm32h747cm7.svd'));
-        cdbg.registerSVDFile(/^(STM|stm)32(h|H)753.*/i, path.join(context.extensionPath, 'data', 'stm32h753.svd'));
-        cdbg.registerSVDFile(/^(STM|stm)32(h|H)753(V|v).*/i, path.join(context.extensionPath, 'data', 'stm32h753v.svd'));
-        cdbg.registerSVDFile(/^(STM|stm)32(h|H)7(B|b)3.*/i, path.join(context.extensionPath, 'data', 'stm32h7b3.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)723.*/i, path.join(context.extensionPath, 'data', 'STM32H723.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)725.*/i, path.join(context.extensionPath, 'data', 'STM32H725.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)730.*/i, path.join(context.extensionPath, 'data', 'STM32H730.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)733.*/i, path.join(context.extensionPath, 'data', 'STM32H733.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)735.*/i, path.join(context.extensionPath, 'data', 'STM32H735.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)742.*/i, path.join(context.extensionPath, 'data', 'STM32H742.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)743.*/i, path.join(context.extensionPath, 'data', 'STM32H743.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)745.*_(CM|cm|M|m)4/i, path.join(context.extensionPath, 'data', 'STM32H745_CM4.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)745.*_(CM|cm|M|m)7/i, path.join(context.extensionPath, 'data', 'STM32H745_CM7.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)747.*_(CM|cm|M|m)4/i, path.join(context.extensionPath, 'data', 'STM32H747_CM4.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)747.*_(CM|cm|M|m)7/i, path.join(context.extensionPath, 'data', 'STM32H747_CM7.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)750.*/i, path.join(context.extensionPath, 'data', 'STM32H750.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)753.*/i, path.join(context.extensionPath, 'data', 'STM32H753.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)755.*_(CM|cm|M|m)4/i, path.join(context.extensionPath, 'data', 'STM32H755_CM4.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)755.*_(CM|cm|M|m)7/i, path.join(context.extensionPath, 'data', 'STM32H755_CM7.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)757.*_(CM|cm|M|m)4/i, path.join(context.extensionPath, 'data', 'STM32H757_CM4.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)757.*_(CM|cm|M|m)7/i, path.join(context.extensionPath, 'data', 'STM32H757_CM7.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)7(A|a)3.*/i, path.join(context.extensionPath, 'data', 'STM32H7A3.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)7(B|b)3.*/i, path.join(context.extensionPath, 'data', 'STM32H7B3.svd'));
+        cdbg.registerSVDFile(/^(STM|stm)32(h|H)7(B|b)0.*/i, path.join(context.extensionPath, 'data', 'STM32H7B0.svd'));
     }, (error) => {
         console.log('Unable to activate cortexDebug');
     });
